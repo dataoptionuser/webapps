5bf6df74755e621b354f47c91f1ff86dd7fea831
thakkde <77808828+thakkde@users.noreply.github.com>
2022-02-15 22:07:03 -0500

    Create longgap.java

---
+++ longgap.java
@@ -0,0 +1,53 @@
+//binary gap
+package javacode;
+
+public class longgap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		solution(1041);
+
+	}
+
+	    public static int solution(int N) {
+	    	 String binstr = Integer.toBinaryString(N);
+	         int len = 0;
+	         int maxlen = 0;
+	         //System.out.println(binstr);
+	         char[] binarr = binstr.toCharArray();
+	         for(int i=0; i<binarr.length; i++)
+	         {
+	             if(i==0)
+	             {
+	                len = 0;
+	                maxlen = 0;
+	             }
+	             else
+	             {
+	                 if(binarr[i] == '1')
+	                 {
+	                    if(binarr[i-1] == '0')
+	                    {
+	                        //current maxlenght 
+	                        if(len > maxlen)
+	                        {
+	                             maxlen = len;
+	                        }
+	                        //reset lenght to 0
+	                        len = 0;
+	                    }
+	                 }
+	                 else
+	                 {
+	                     len = len + 1;
+	                 }
+	             }
+	         }
+	         return maxlen;
+
+	    }
+	
+	
+	
+
+}
